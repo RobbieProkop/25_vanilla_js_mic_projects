c64054eb7522f337aac6772ae231b2449b04d561
Robbie Prokop <robbieprokop@gmail.com>
2022-11-10 15:51:12 +0400

    beign creating calc func

--- exchange_rate_calc/app.js
+++ exchange_rate_calc/app.js
@@ -8,11 +8,21 @@ const rate = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
 //functions
-const calculate = async (currency) => {
-  const res = await fetch(
-    `https://v6.exchangerate-api.com/v6/dc7e381b18f065120774ccd1/latest/${currency}`
-  );
-  console.log("res :>> ", res.json());
+//fetch exchange rates and update UI
+const calculate = async () => {
+  const currency1Value = currency1.value;
+  const currency2Value = currency2.value;
+  console.log("currency1Value :>> ", currency1Value, currency2Value);
+  // const res = await fetch(
+  //   `https://v6.exchangerate-api.com/v6/dc7e381b18f065120774ccd1/latest/${currency}`
+  // );
+  // console.log("res :>> ", res.json());
 };
+
+//event listeners
+currency1.addEventListener("change", calculate);
+amount1.addEventListener("input", calculate);
+currency2.addEventListener("change", calculate);
+amount2.addEventListener("input", calculate);
 
 calculate("USD");
