8a5051dddad0cd96ef7e744343204ad6ff47100b
Robbie Prokop <robbieprokop@gmail.com>
2022-11-10 16:51:24 +0400

    begin JS

---
+++ dom_array_methods/app.js
@@ -0,0 +1,8 @@
+// dom elements
+
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add_user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show_millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateBtn = document.getElementById("calculate_wealth");
--- dom_array_methods/readme.md
+++ dom_array_methods/readme.md
@@ -1,5 +1,5 @@
 # DOM Array Method Practice
 
-### Built entirely with Vanilla JS, HTML, and SASS
+### Built entirely with Vanilla JS, HTML, and CSS
 
 <img src="https://github.com/RobbieProkop/25_vanilla_js_micro_projects/blob/master/exchange_rate_calc/img/exchange-screen.png" />
