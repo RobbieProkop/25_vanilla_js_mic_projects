359be633387f3f61c4ed05c3cf1933e577ee35ca
Robbie Prokop <robbieprokop@gmail.com>
2022-11-10 16:59:37 +0400

    get random user

--- dom_array_methods/app.js
+++ dom_array_methods/app.js
@@ -1,8 +1,28 @@
 // dom elements
-
 const main = document.getElementById("main");
 const addUserBtn = document.getElementById("add_user");
 const doubleBtn = document.getElementById("double");
 const showMillionairesBtn = document.getElementById("show_millionaires");
 const sortBtn = document.getElementById("sort");
 const calculateBtn = document.getElementById("calculate_wealth");
+
+//Data array
+let data = [];
+
+//fetch random user and add money
+const getRandomUser = async () => {
+  const res = await fetch("https://randomuser.me/api");
+
+  const data = await res.json();
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+  console.log("newUser :>> ", newUser);
+};
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
