0a4ae2b6f88e7768e745f1080e971807f933e8b4
Robbie Prokop <robbieprokop@gmail.com>
2022-11-26 12:20:14 +0400

    create array of words

--- hangman/app.js
+++ hangman/app.js
@@ -4,3 +4,17 @@ const playAgainBtn = document.getElementById("play-again");
 const popup = document.getElementById("popup");
 const notification = document.getElementById("notification-container");
 const finalMessage = document.getElementById("final-message");
+
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = [
+  "application",
+  "data structure",
+  "programmer",
+  "web developer",
+  "meme time",
+];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+console.log("selectedWords", selectedWord);
