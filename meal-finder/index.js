113b378da585571a4528a3bb27857dd276d0de9a
Robbie Prokop <robbieprokop@gmail.com>
2022-11-30 17:50:21 +0400

    create an alert

--- meal-finder/index.js
+++ meal-finder/index.js
@@ -4,6 +4,17 @@ const random = document.getElementById("random");
 const meals = document.getElementById("meals");
 const result = document.getElementById("result-heading");
 const single = document.getElementById("single-meal");
+const formContainer = document.getElementById("form-container");
+
+const notify = () => {
+  const alert = document.createElement("h2");
+  alert.classList.add("error");
+  alert.innerHTML = "Please Enter a Search";
+  formContainer.parentElement.insertBefore(alert, formContainer);
+  setTimeout(() => {
+    document.querySelector(".error").remove();
+  }, 2000);
+};
 
 // searhc meal and fetch from api
 const searchMeal = (e) => {
@@ -14,6 +25,11 @@ const searchMeal = (e) => {
 
   // get search term
   const term = search.value;
+
+  // check for empty submit
+  if (!term.trim()) {
+    notify();
+  }
 
   console.log("term :>> ", term);
 };
