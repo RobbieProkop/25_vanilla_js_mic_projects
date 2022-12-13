826973d5f72c084c94500c856e7d0040816a01c9
Robbie Prokop <robbieprokop@gmail.com>
2022-12-13 12:54:22 -0700

    fix html in index.js

--- meal-finder/index.js
+++ meal-finder/index.js
@@ -44,11 +44,11 @@ const searchMeal = (e) => {
           (meal) =>
             `
            <div class="meal">
-            <img src="${meal.strMealThumb}" alt="${meal.strMeal} />
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal} "/>
             <div class="meal-info" data-mealID="${meal.idMeal}" >
               <h3>${meal.strMeal}</h3>
-            </div
-           <div>
+            </div>
+           </div>
           `
         )
         .join("");
